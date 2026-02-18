938a5efdee34607b4ddc39585f398075d89f119d
Peterlits Zo <zhouhongyu@ft.tech>
2026-02-19 00:43:17 +0800

    feat: Set minimum window size.

--- src/main.ts
+++ src/main.ts
@@ -17,8 +17,10 @@ if (started) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 900,
+    minWidth: 800,
+    minHeight: 600,
     webPreferences: {
       contextIsolation: true,
       nodeIntegration: false,
